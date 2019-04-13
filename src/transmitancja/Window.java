935a3cde5ac4e239939b6caa6f84d17cb5ffbc65
jakub2103 <40215951+jakub2103@users.noreply.github.com>
2019-04-13 14:05:17 +0200

    Nic za bardzo nie zmieniłem

--- src/transmitancja/Window.java
+++ src/transmitancja/Window.java
@@ -346,3 +346,4 @@ public class Window extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
+//Ala ma kota a kuba bawi się git
